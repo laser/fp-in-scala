30939469390312b962b2fab86b53b23d71db44d9
Erin Swenson-Healey <l@s3r.me>
2015-09-27 16:27:39 -0500

    Continue data structure solution for week 3

--- src/main/scala/homework/Week03.scala
+++ src/main/scala/homework/Week03.scala
@@ -49,24 +49,97 @@ object Week03 {
   // Exercise 3.7, page 40
   // Exercise 3.8, page 40
   // Exercise 3.9, page 40
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case x::xs => f(x, foldRight(xs, z)(f)) 
+  }
+
+  def length[A](as: List[A]): Int = foldRight(as, 0)((_, acc: Int) => acc + 1)
+
   // Exercise 3.10, page 40
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case x::xs => foldLeft(xs, f(z, x))(f)
+  }
+
   // Exercise 3.11, page 41
+  def sum(is: List[Int]): Int = foldLeft(is, 0)(_+_) 
+  def product(is: List[Int]): Int = foldLeft(is, 1)(_*_)
+  def length2(as: List[Int]): Int = foldLeft(as, 0)((acc: Int, _) => acc + 1)
+  
   // Exercise 3.12, page 41
+  def reverse[A](as: List[A]): List[A] = foldLeft(as, List[A]())((acc: List[A], item: A) => item :: acc)
+
   // Exercise 3.13, page 41
+  def foldLeftViaFoldRight[A, B](as: List[A], z: B)(f: (B, A) => B): B = ???
+  def foldRightViaFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = ???
+
   // Exercise 3.14, page 41
+  def append[A](as1: List[A], as2: List[A]): List[A] = foldRight(as1, as2)((item: A, acc: List[A]) => item :: acc)
+
   // Exercise 3.15, page 41
+  def flatten[A](lols: List[List[A]]): List[A] =
+    foldRight(lols, List[A]())((item: List[A], acc: List[A]) => foldRight(item, acc)(_::_))
+
   // Exercise 3.16, page 42
+  //def incrementEachByOne(is: List[Int]): List[Int] = foldRight(is, List[Int]())(_+1::_)
+  def incrementEachByOne(is: List[Int]): List[Int] = map(is)(_+1)
+
   // Exercise 3.17, page 42
+  //def doublesToStrings(ds: List[Double]): List[String] = foldRight(ds, List[String]())(_.toString::_)
+  def doublesToStrings(ds: List[Double]): List[String] = map(ds)(_.toString)
+
   // Exercise 3.18, page 42
+  def map[A,B](as: List[A])(f: A => B): List[B] = foldRight(as, List[B]())(f(_)::_)
+
   // Exercise 3.19, page 42
+  def filter[A](as: List[A])(p: A => Boolean): List[A] =
+    foldRight(as, List[A]())((item: A, acc: List[A]) => if (p(item)) item::acc else acc)
+
   // Exercise 3.20, page 42
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] =
+    foldRight(as, List[B]())((item: A, acc: List[B]) => foldRight(f(item), acc)(_::_))
+
   // Exercise 3.21, page 43
+  def filterViaFlatMap[A](as: List[A])(p: A => Boolean): List[A] = flatMap(as) { item =>
+    if (p(item)) List(item)
+    else Nil
+  }
+
   // Exercise 3.22, page 43
+  def zipWithAdd(is1: List[Int], is2: List[Int]): List[Int] = (is1, is2) match {
+    case (x::xs, y::ys) => (x+y) :: zipWithAdd(xs, ys)
+    case _              => List[Int]()
+  }
+
   // Exercise 3.23, page 43
+  def zipWith[A](as1: List[A], as2: List[A])(f: (A, A) => A): List[A] = (as1, as2) match {
+    case (x::xs, y::ys) => f(x, y) :: zipWith(xs, ys)(f)
+    case _              => List[A]()
+  }
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
   // Exercise 3.24, page 44
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = ???
+
   // Exercise 3.25, page 46
+  def treeSize[A](t: Tree[A]): Int = ???
+
   // Exercise 3.26, page 46
+  def intTreeMax(t: Tree[Int]): Int = ???
+
   // Exercise 3.27, page 46
+  def treeDepth[A](t: Tree[A]): Int = ???
+
   // Exercise 3.28, page 46
+  def treeMap[A, B](t: Tree[A])(f: A => B): Tree[B] = treeFold((v: A) => Leaf(f(v)): Tree[B])(Branch(_,_))(t)
+
   // Exercise 3.29, page 47
+  def treeFold[A, B](l: A => B)(b: (B, B) => B)(t: Tree[A]): B = t match {
+    case Leaf(value) => l(value)
+    case Branch(left, right) => b(treeFold(l)(b)(left), treeFold(l)(b)(right))
+  }
 }
--- src/test/scala/homework/Week03Spec.scala
+++ src/test/scala/homework/Week03Spec.scala
@@ -1,12 +1,14 @@
 package homework 
 
-import org.scalacheck.Gen.{ listOfN, posNum }
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Gen.{ listOfN, posNum, oneOf }
 import org.scalacheck.Prop.{ forAll }
 import org.scalacheck.Properties
 import org.specs2.mutable._
 import org.specs2.ScalaCheck
 
-import homework.Week03.{ init, tail, setHead, drop, dropWhile }
+import homework.Week03._
 
 object G {
   val nonEmptyIntList = for {
@@ -48,16 +50,97 @@ object ListDropWhileProp extends Properties("dropWhile") {
 }
 
 object ListInitProp extends Properties("init") {
-  property(".. dunno") = forAll(G.nonEmptyIntList) { ns =>
+  property("..") = forAll(G.nonEmptyIntList) { ns =>
     init(ns).length == ns.length-1
   }
 }
 
+object ListLengthProp extends Properties("length") {
+  property(".. behaves identically to the provided length method") = forAll(G.nonEmptyIntList) { ns =>
+    length(ns) == ns.length
+  }
+}
+
+object ListReverseProp extends Properties("reverse") {
+  property(".. behaves identically to the provided reverse method") = forAll { ns: List[Int] =>
+    reverse(ns) == ns.reverse
+  }
+}
+
+object ListAppendProp extends Properties("append") {
+  property(".. behaves identically to ++ over list") = forAll { (ss1: List[String], ss2: List[String]) =>
+    append(ss1, ss2) == ss1 ++ ss2
+  }
+}
+
+object ListFlattenProp extends Properties("flatten") {
+  property(".. behaves identically to flatten over list") = forAll { lols: List[List[String]] =>
+    flatten(lols) == lols.flatten
+  }
+}
+
+object ListIncrementByOneProp extends Properties("increment by one") {
+  property("..") = forAll { ns: List[Int] => 
+    incrementEachByOne(ns).length == ns.length
+    sum(incrementEachByOne(ns)) == (sum(ns) + ns.length)
+  }
+}
+
+object ListFilterViaFlatMapProp extends Properties("filterViaFlatMap") {
+  val predicate = (i: Int) => i > 0
+
+  property(".. behaves identically to filter member") = forAll { is: List[Int] =>
+    is.filter(predicate) == filterViaFlatMap(is)(predicate)
+  }
+
+  property(".. behaves identically to our filter method") = forAll { is: List[Int] =>
+    filter(is)(predicate) == filterViaFlatMap(is)(predicate)
+  }
+}
 
 object Week03Spec extends Specification {
   "init" should {
     "preserve the original ordering" in {
       init(List[Int](1,2,3,4)) mustEqual List[Int](1,2,3)
+    }
+  }
+
+  "flatMap" should {
+    "work?" in {
+      flatMap(List[Int](1,2,3)) { item =>
+        List[Int](item, item, item)
+      } mustEqual List[Int](1,1,1,2,2,2,3,3,3)
+    }
+  }
+
+  "treeDepth" should {
+    "give the depth of the deepest node in the tree" in {
+      val t1: Tree[Int] = Leaf(4)
+      treeDepth(t1) mustEqual(0)
+
+      val t2: Tree[Int] = Branch(Leaf(5), Leaf(1))
+      treeDepth(t2) mustEqual(1)
+
+      val t3: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(10))
+      treeDepth(t3) mustEqual(2)
+    }
+  }
+
+  "treeFold" should {
+    "perform a left-to-right, depth-first traversal of nodes" in {
+      val t1: Tree[Int] = Leaf(4)
+      treeFold((v: Int) => v.toString)((left: String, right: String) => left + right)(t1) mustEqual "4"
+
+      val t2: Tree[String] = Branch(Leaf("01"), Branch(Branch(Leaf("02"), Leaf("03")), Leaf("04")))
+      treeFold((v: String) => v)((left: String, right: String) => left + right)(t2) mustEqual "01020304"
+    }
+
+    "using constructors results in identity of tree" in {
+      def mkLeaf(v: String): Tree[String] = Leaf(v)
+      def mkBranch(left: Tree[String], right: Tree[String]): Tree[String] = Branch(left, right)
+
+      val t: Tree[String] = Branch(Leaf("01"), Branch(Branch(Leaf("02"), Leaf("03")), Leaf("04")))
+      treeFold(mkLeaf _)(mkBranch _)(t) == t
     }
   }
 }
