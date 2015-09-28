f83e920e98394d6907c36fcbb64ebb99f0068698
Erin Swenson-Healey <l@s3r.me>
2015-09-27 21:40:27 -0700

    Complete Week 4 exercises up to Either trait

--- src/main/scala/homework/Week04.scala
+++ src/main/scala/homework/Week04.scala
@@ -61,19 +61,36 @@ object Week04 {
   def sequenceViaTraverse[A](mas: List[Maybe[A]]): Maybe[List[A]] = traverse(mas)(id)
 
   // Exercise 4.6, page 62
-  sealed trait Either[+E, +A] { self =>
-    def map[B](f: A => B): Either[E, B] = ???
-    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
-    def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = ???
-    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  sealed trait Either2[+E, +A] { self =>
+    def map[B](f: A => B): Either2[E, B] = self match {
+      case Right(x) => Right(f(x))
+      case Left(x) => Left(x)
+    }
+
+    def flatMap[EE >: E, B](f: A => Either2[EE, B]): Either2[EE, B] = self match {
+      case Right(x) => f(x)
+      case Left(x) => Left(x)
+    }
+
+    def orElse[EE >: E,B >: A](b: => Either2[EE, B]): Either2[EE, B] = self match {
+      case Right(x) => Right(x)
+      case Left(x) => b
+    }
+
+    def map2[EE >: E, B, C](b: Either2[EE, B])(f: (A, B) => C): Either2[EE, C] = (self, b) match {
+      case (Right(x), Right(y)) => Right(f(x, y))
+      case (Left(x), _)         => Left(x)
+      case (_, Left(x))         => Left(x)
+    }
   }
-  case class Left[+E](value: E) extends Either[E, Nothing] 
-  case class Right[+A](value: A) extends Either[Nothing, A]
+
+  case class Left[+E](value: E) extends Either2[E, Nothing] 
+  case class Right[+A](value: A) extends Either2[Nothing, A]
 
   // Exercise 4.7, page 62
-  //def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = ???
+  //def sequence[E, A](es: List[Either2[E, A]]): Either2[E, List[A]] = ???
 
-  //def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  //def traverse[E, A, B](as: List[A])(f: A => Either2[E, B]): Either2[E, List[B]] = ???
 
   // Exercise 4.8, page 63
   //
