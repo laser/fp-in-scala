525d0e60e039a60553d627ee1a4cc0e952218884
Erin Swenson-Healey <l@s3r.me>
2015-10-03 16:03:53 -0700

    Continue Week 5 implementation

--- src/main/scala/homework/week5/Stream.scala
+++ src/main/scala/homework/week5/Stream.scala
@@ -5,10 +5,10 @@ import scala.annotation.tailrec
 import homework.Stream._
 
 sealed trait Stream[+A] { self =>
-  def headOption: Option[A] = self match {
-    case Empty => None
-    case Cons(h, t) => Some(h())
-  }
+  //def headOption: Option[A] = self match {
+    //case Empty => None
+    //case Cons(h, t) => Some(h())
+  //}
 
   // Exercise 5.1
   def toList: List[A] = {
@@ -22,16 +22,10 @@ sealed trait Stream[+A] { self =>
   }
 
   // Exercise 5.2a
-  def take(n: Int): Stream[A] = {
-    @tailrec
-    def go(acc: Stream[A], counter: Int, as: Stream[A]): Stream[A] =
-      if (counter == n) acc
-      else as match {
-        case Empty => acc
-        case Cons(h, t) => go(cons(h(), acc), counter+1, t())
-      }
-
-    go(Empty, 0, self)
+  def take(n: Int): Stream[A] =  self match {
+    case Empty => Empty
+    case Cons(_, _) if (n < 1) => Empty
+    case Cons(h, t) => cons(h(), t().take(n-1))
   }
 
   // Exercise 5.2b
@@ -46,6 +40,65 @@ sealed trait Stream[+A] { self =>
 
     go(0, self)
   }
+
+  // Exercise 5.3
+  // see 5.5 below
+  //def takeWhile(p: A => Boolean): Stream[A] = self match {
+    //case Empty => Empty
+    //case Cons(h, t) => if (p(h())) cons(h(), t().takeWhile(p))
+                       //else Empty
+  //}
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = self match {
+    case Empty => z
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+  }
+
+  // Exercise 5.4
+  def forAll(p: A => Boolean): Boolean = self.foldRight(true)(p(_) && _)
+
+  // Exercise 5.5
+  def takeWhile(p: A => Boolean): Stream[A] = self.foldRight(Empty:Stream[A]){ (item, acc) =>
+    if (p(item)) cons(item, acc) else Empty
+  }
+
+  // Exercise 5.6
+  def headOption: Option[A] = self.foldRight(None:Option[A])((x, _) => Some(x))
+
+  // Exercise 5.7
+  // map, filter, append, and flatMap using foldRight
+  def map[B](f: A => B): Stream[B] =
+    self.foldRight(Empty:Stream[B])((x, z) => cons(f(x), z))
+
+  def filter(p: A => Boolean): Stream[A] =
+    self.foldRight(Empty:Stream[A])((x, z) => if (p(x)) cons(x, z) else z)
+
+  def append[B>:A](x: => B): Stream[B] =
+    self.foldRight(cons(x, empty))((x, z) => cons(x, z))
+
+  def concat[B>:A](s: => Stream[B]): Stream[B] =
+    self.foldRight(s)((x, z) => cons(x, z))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    self.foldRight(empty[B])((x, z) => f(x).foldRight(empty[B])((x2, z2) => cons(x2, z2)))
+
+  // Exercise 5.13
+  // use unfold to implement map, take, takeWhile, zipWith, and zipAll
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = ???
+
+  // Exercise 5.14
+  // implement startsWith using function's you've written
+  // Stream(1,2,3) startsWith Stream(1,2) would be true
+  def startsWith[A](s: Stream[A]): Boolean = ???
+
+  // Exercise 5.15
+  // implement tails using unfold
+  def tails: Stream[Stream[A]] = ???
+
+  //def hasSubsequence[A](s: Stream[A]): Boolean = tails exists (_ startsWith s)
+
+  // Exercise 5.16
+  // generalize tails to scanRight
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -62,8 +115,40 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
-  def constant[A](a: A): Stream[A] = {
-    lazy val tail: Stream[A] = Cons(() => a, () => tail)
-    tail
+  // Exercise 5.8 (see 5.12)
+  //def constant[A](a: A): Stream[A] = {
+    //lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    //tail
+  //}
+
+  def generate[A](seed: A)(next: A => A): Stream[A] = cons(seed, generate(next(seed))(next))
+
+  // Exercise 5.9 (see 5.12)
+  //def from(n: Int): Stream[Int] = generate(n)(_+1)
+
+  // Exercise 5.10 (see 5.12)
+  //def fibs: Stream[Int] = {
+    //def generate2(seed: (Int, Int))(next: (Int, Int) => (Int, Int)): Stream[(Int, Int)] =
+      //cons(seed, generate2(next.tupled(seed))(next))
+
+    //generate2(0, 1)((x, y) => (y, x+y)).map(_._1)
+  //}
+
+  // Exercise 5.11
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case None => Empty
+    case Some((x, z2)) => cons(x, unfold(z2)(f))
   }
+
+  // Exercise 5.12
+  def fibs: Stream[Int] =
+    Stream(0, 1).concat(unfold((0, 1))((s) => Some((s._1+s._2, (s._2, s._1+s._2)))))
+
+  def from(n: Int): Stream[Int] =
+    unfold(n)((s) => Some(s, s+1))
+
+  def constant[A](x: A): Stream[A] = unfold(x)((s) => Some(s, s))
+
+  def ones: Stream[Int] = unfold(1)(Function.const(Some(1, 1)))
+
 }
--- src/test/scala/homework/Week05StreamSpec.scala
+++ src/test/scala/homework/Week05StreamSpec.scala
@@ -5,10 +5,20 @@ import org.specs2.mutable._
 import homework.Stream._
 
 object Week05StreamSpec extends Specification {
+  "headOption" should {
+    ".." in {
+      val s1 = Empty:Stream[Int]
+      val s2 = Stream(1,2,3)
+
+      s1.headOption mustEqual None
+      s2.headOption mustEqual Some(1)
+    }
+  }
+
   "take" should {
     ".." in {
-      val s1 = constant(1)
-      s1.take(5).toList mustEqual List(1,1,1,1,1)
+      val s1 = Stream(1,2,3,4,5,6)
+      s1.take(3).toList mustEqual List(1,2,3)
 
       val s2 = Empty
       s2.take(10).toList mustEqual List()
@@ -25,6 +35,51 @@ object Week05StreamSpec extends Specification {
 
       val s3 = constant(1)
       s3.drop(1).take(4).toList mustEqual List(1,1,1,1)
+    }
+  }
+
+  "takeWhile" should {
+    ".." in {
+      val expensive = (x: Int) => () => { println(x); x }
+
+      val s1 = Stream(expensive(1), expensive(2), expensive(3), expensive(4), expensive(5))
+
+      s1.takeWhile(f => f() < 2).toList.map(_()) mustEqual List(1)
+    }
+  }
+
+  "generate" should {
+    ".." in {
+      val s1 = generate(1)(_+1)
+
+      s1.take(5).toList mustEqual List(1,2,3,4,5)
+    }
+  }
+
+  "foldRight" should {
+    ".." in {
+      val s1 = constant(true)
+      s1.foldRight(false)(_||_) mustEqual true
+    }
+  }
+
+  "forAll" should {
+    ".." in {
+      val s1 = Stream(1,2,3,4,5)
+      s1.forAll(_ > 0) mustEqual true
+    }
+  }
+
+  "filter" should {
+    ".." in {
+      val s1 = Stream(1,2,3,4,5)
+      s1.filter(_ < 4).toList mustEqual List(1,2,3)
+    }
+  }
+
+  "fibs" should {
+    ".. not blow the stack" in {
+      fibs.take(5).toList mustEqual List(0, 1, 1, 2, 3)
     }
   }
 }
